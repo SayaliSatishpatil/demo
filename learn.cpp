b2c00dee7d7eec2ee653f9e835e2b2a6ee826f80
SayaliSatishpatil <sayalipatil5045@gmail.com>
2024-07-12 17:59:25 +0530

    meaningful message

---
+++ learn.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+map<int,pair<int,int>> map;
+pair<int,pair<int, int>> vec = 
+    {2,{1, 50}};
+
+cout<<vec.first<<endl<<vec.second<<endl;
+
+    return 0;
+}
